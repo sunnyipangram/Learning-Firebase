bc2ebde1ce62b386b4ca9f8b036a7a5159f7ce81
Sunny Jadaun <sunnyjadaun@ipangram.com>
2023-08-18 18:04:34 +0530

    Diffrent signin Method with Create Read delete functionality

--- src/App.js
+++ src/App.js
@@ -1,101 +1,178 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useEffect, useState} from 'react'
-import { app,auth,googleProvider,db } from './FirebaseConfig';
-import { createUserWithEmailAndPassword,signInWithPopup,signOut } from 'firebase/auth';
-import {getDocs,collection} from 'firebase/firestore'
-
+import React, { useEffect, useState } from 'react';
+import { auth, googleProvider, db } from './FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { getDocs, collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import {AiFillDelete, AiOutlineDelete} from 'react-icons/ai'
 
 function App() {
-  const [Data, setData] = useState({email:'', password:''})
-  const [MovieList, setMovieList] = useState([])
+  const [Data, setData] = useState({ email: '', password: '' });
+  const [MovieList, setMovieList] = useState([]);
+  const [AddMovieData, setAddMovieData] = useState({ title: '', date: 0, oscar: false });
 
-  
+  useEffect(() => {
+    GetMovieList();
+  }, []);
 
-useEffect(() => {
-  
-  GetMovieList()
-}, [])
+  const movieCollectionRef = collection(db, 'movies');
 
-const movieCollectionRef=collection(db,"movies")
-  const GetMovieList=async()=> {
-    console.log('GetMovieList')
-    //Read the movie list
-    //setMovieList(MovieList)
-try{
- let response= await getDocs(movieCollectionRef)
- let filteredData=response.docs.map((doc)=>({...doc.data(),id:doc.id}))
- 
-console.log(filteredData)
-
-}
-catch(err){
-  console.error(err.message)
-
-}
-  }
+  const GetMovieList = async () => {
+    try {
+      const response = await getDocs(movieCollectionRef);
+      const filteredData = response.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   const handleOnchange = (e) => {
-    console.log(e.target.value)
-     let {name, value} = e.target;
-     setData((preValue)=>({...preValue, [name]: value}));
-     
+    const { name, value } = e.target;
+    setData((preValue) => ({ ...preValue, [name]: value }));
+  };
 
-  }
-  const signIn=async(e)=>{
-    e.preventDefault()
-    try{
-      await createUserWithEmailAndPassword(auth,Data.email,Data.password)
-      console.log('user created')
+  const signIn = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, Data.email, Data.password);
+      console.log('User created');
+    } catch (err) {
+      console.log('Error: Could not create user', err.message);
     }
-    catch(err){
-      console.log('Error wrong input could not be create user',err.message)
+  };
+
+  const LogOut = async (e) => {
+    e.preventDefault();
+    try {
+      await signOut(auth);
+      console.log('User signed out');
+    } catch (err) {
+      console.log(err.message);
     }
-    
+  };
 
-  }
-  const LogOut=async(e) => {
-    e.preventDefault()
-    try{
-      await signOut(auth)
-      console.log('user signed out')
+  const signInWithGoogle = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithPopup(auth, googleProvider);
+      console.log('User signed up');
+    } catch (err) {
+      console.log('Could not sign in', err.message);
     }
-    catch(err) {
-      console.log(err.message)
+  };
+
+  const handleMovieDataChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setAddMovieData((preValue) => ({ ...preValue, [name]: newValue }));
+  };
+
+  const onSubmitMovie = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(movieCollectionRef, {
+        title: AddMovieData.title,
+        releaseDate: AddMovieData.date,
+        recievedAnOscar: AddMovieData.oscar,
+      });
+      GetMovieList();
+      console.log('Movie added successfully');
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
+  const deleteMovie = async (id) => {
+    try {
+      const movieDocRef = doc(db, 'movies', id);
+      await deleteDoc(movieDocRef);
+      GetMovieList();
+      console.log('Movie deleted successfully');
+    } catch (err) {
+      console.error('Error deleting movie', err);
+    }
+  };
+  
 
-  }
-
-  // console.log(auth?.currentUser?.email===undefined?"No User   In":auth?.currentUser?.email)
-
-console.log(auth?.currentUser?.email)
-
-  const signInWithGoogle = async(e) => {
-    e.preventDefault()
-    
-try{
-  await signInWithPopup(auth,googleProvider)
-  console.log('user signed up')
-
-}
-catch(err){
-  console.log(err.message,'could not sign in')
-}
-  }
-
-  // console.log(auth?.currentUser?.email ,"hfhjdfh")
   return (
     <div className="App">
-     
-      <form action="" >
-      <input type="email" name="email" id="" value={Data.email} onChange={handleOnchange}  className="email-input" />
-      <input type="password" name="password" value={Data.password} onChange={handleOnchange}    id="" />
-      <button onClick={signIn}>Simple SignIn</button>
-      <button onClick={signInWithGoogle}>SignIn with Google</button>
-      <button onClick={LogOut}>Logout</button>
+      <form action="">
+        <input
+          type="email"
+          name="email"
+          id=""
+          value={Data.email}
+          onChange={handleOnchange}
+          className="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          value={Data.password}
+          onChange={handleOnchange}
+          id=""
+        />
+        <button onClick={signIn}>Simple SignIn</button>
+        <button onClick={signInWithGoogle}>SignIn with Google</button>
+        <button onClick={LogOut}>Logout</button>
       </form>
-
+      <form action="">
+        <div>
+          <input
+            type="text"
+            className="movie-name"
+            name="title"
+            value={AddMovieData.title}
+            onChange={handleMovieDataChange}
+            placeholder="movie title..."
+          />
+          <input
+            type="number"
+            className="release-date"
+            name="date"
+            value={AddMovieData.date}
+            onChange={handleMovieDataChange}
+          />
+          <input
+            type="checkbox"
+            name="oscar"
+            checked={AddMovieData.oscar}
+            onChange={handleMovieDataChange}
+          />
+          <label htmlFor="">Recieved An Oscar</label>
+          <button onClick={onSubmitMovie}>Submit Movie</button>
+        </div>
+      </form>
+      <div>
+        <table>
+          <caption>Statement Summary</caption>
+          <thead>
+            <tr>
+              <th scope="col">Movie Name</th>
+              <th scope="col">Release Date</th>
+              <th scope="col">Oscar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {MovieList.map((Movie) => (
+              <tr key={Movie.id}>
+                <td
+                  data-label="Account"
+                  style={{ color: Movie.recievedAnOscar ? 'green' : 'red' }}
+                >
+                  {Movie.title}
+                </td>
+                <td data-label="Due Date">{Movie.releaseDate}</td>
+                <td data-label="Amount">{Movie.recievedAnOscar ? 'Yes' : 'No'}</td>
+                <td data-label="Amount">
+                  <button style={{color:'red',border:'none',outline:'0',borderRadius:'50%',width:'25px',height:'25px',fontSize:'20px'}} onClick={() => deleteMovie(Movie.id)}><AiFillDelete/></button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
